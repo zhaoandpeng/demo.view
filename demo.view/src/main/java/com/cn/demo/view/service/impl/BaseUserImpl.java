003ab4c481d0168a248ff60cb0e13bbeaf8dfe85
006 <006@006-PC>
2019-04-22 17:12:06 +0800

    完善基层代码

--- demo.view/src/main/java/com/cn/demo/view/service/impl/BaseUserImpl.java
+++ demo.view/src/main/java/com/cn/demo/view/service/impl/BaseUserImpl.java
@@ -7,7 +7,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.util.Assert;
 
 import com.cn.demo.view.dao.BaseUserDao;
 import com.cn.demo.view.model.BaseUser;
