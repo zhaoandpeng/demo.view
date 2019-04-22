1c8a9eb7ab6c2ef8f9aa3a5cf4f9ecb2522451bd
006 <006@006-PC>
2019-04-22 16:35:59 +0800

    修改基础业务代码

--- demo.view/src/main/java/com/cn/demo/view/model/BaseUser.java
+++ demo.view/src/main/java/com/cn/demo/view/model/BaseUser.java
@@ -1,12 +1,19 @@
 package com.cn.demo.view.model;
 
 import java.io.Serializable;
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.cn.demo.view.annotation.TableInfoAnnotation;
 
 /**
  * @author Mr.Zhao
  * @date 2019-03-28
  */
-public class BaseUser implements Serializable{
+@TableInfoAnnotation(tableName = "base_user", primaryKey = "id")
+public class BaseUser implements Serializable, UserDetails{
 	
 	private static final long serialVersionUID = -828643532655725840L;
 
@@ -109,4 +116,34 @@ public class BaseUser implements Serializable{
     public void setAge(Integer age) {
         this.age = age;
     }
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
+	}
 }
