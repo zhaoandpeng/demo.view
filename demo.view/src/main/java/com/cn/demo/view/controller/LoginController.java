625ded852247a5d1095ddd950b887de7642b4981
006 <006@006-PC>
2019-04-22 17:12:40 +0800

    完善基层代码

--- demo.view/src/main/java/com/cn/demo/view/controller/LoginController.java
+++ demo.view/src/main/java/com/cn/demo/view/controller/LoginController.java
@@ -5,17 +5,22 @@ import javax.annotation.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.cn.demo.view.service.BaseUserService;
+import com.cn.demo.view.service.BaseRoleService;
 
 @Controller
 public class LoginController{
 
 	
 	@Resource
-	private BaseUserService baseUserService;
+	private BaseRoleService baseRoleService;
 	
 	@RequestMapping(value="/main") 
 	public String home() {
+		
+		//加载用户树形菜单列表
+		
+		
+		
 		
 		return "main/main";
 	}
---
+++ demo.view/src/main/java/com/cn/demo/view/service/BaseRoleService.java
@@ -0,0 +1,7 @@
+package com.cn.demo.view.service;
+
+import com.cn.demo.view.model.BaseRole;
+
+public interface BaseRoleService extends BaseService<BaseRole, java.lang.String>{
+
+}
