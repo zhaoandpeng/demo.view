0b50ac76945e791eb5ed1c17f3858912ddfb33b9
006 <006@006-PC>
2019-04-22 16:38:17 +0800

    修改基础业务代码

--- demo.view/src/main/java/com/cn/demo/view/controller/LoginController.java
+++ demo.view/src/main/java/com/cn/demo/view/controller/LoginController.java
@@ -3,7 +3,6 @@ package com.cn.demo.view.controller;
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.cn.demo.view.service.BaseUserService;
@@ -15,33 +14,11 @@ public class LoginController{
 	@Resource
 	private BaseUserService baseUserService;
 	
-	/**
-	 * <p>Method: index</p>  
-	 * <p>Description: 登录页 </p>  
-	 */
-	@RequestMapping(value="/index")
-	public String index() {
-		
-		return "index";
-		
-	}
-	
-	
-	@GetMapping(value="/login")
-	public String login() {
-		
-		
-		return "main/main";
-	}
-	
-	
-	@RequestMapping(value="/main")
+	@RequestMapping(value="/main") 
 	public String home() {
-		System.out.println("homehomehomehomehomehomehomehomehomehomehomehomehomehome");
-		
 		
 		return "main/main";
-		
 	}
+	 
 	
 }
--- demo.view/src/main/java/com/cn/demo/view/service/impl/BaseUserImpl.java
+++ demo.view/src/main/java/com/cn/demo/view/service/impl/BaseUserImpl.java
@@ -1,11 +1,47 @@
 package com.cn.demo.view.service.impl;
 
-import org.springframework.stereotype.Service;
+import javax.annotation.Resource;
 
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+
+import com.cn.demo.view.dao.BaseUserDao;
 import com.cn.demo.view.model.BaseUser;
 import com.cn.demo.view.service.BaseUserService;
 
 @Service
-public class BaseUserImpl extends BaseServiceImpl<BaseUser,java.lang.String> implements BaseUserService{
+public class BaseUserImpl extends BaseServiceImpl<BaseUser,java.lang.String> implements BaseUserService, UserDetailsService{
+	
+	
+	
+	@Resource
+	private BaseUserDao baseUserDao;
+	
+	
+	@Resource 
+	private PasswordEncoder passwordEncoder;
+	 
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		BaseUser baseUser = null;
+		
+		baseUser = this.baseUserDao.getBaseUserByParam(username);
+		
+		if(null==baseUser) {
+			
+			//自定义提示查无此用户
+		}
+		
+		baseUser.setPassword(passwordEncoder.encode(baseUser.getPassword()));
+		
+		return baseUser;
+	}
+
 
 }
