4eff551bd7b2abd66b70bbf91552d0cf1b7ff329
006 <006@006-PC>
2019-04-24 09:09:08 +0800

    add

---
+++ demo.view/src/main/java/com/cn/demo/view/dao/BaseUserRoleDao.java
@@ -0,0 +1,7 @@
+package com.cn.demo.view.dao;
+
+import com.cn.demo.view.model.BaseUserRole;
+
+public interface BaseUserRoleDao extends BaseDao<BaseUserRole, java.lang.String>{
+
+}
---
+++ demo.view/src/main/java/com/cn/demo/view/dao/impl/BaseUserRoleDaoImpl.java
@@ -0,0 +1,10 @@
+package com.cn.demo.view.dao.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.cn.demo.view.dao.BaseUserRoleDao;
+import com.cn.demo.view.model.BaseUserRole;
+@Service
+public class BaseUserRoleDaoImpl extends BaseDaoImpl<BaseUserRole,java.lang.String> implements BaseUserRoleDao{
+
+}
